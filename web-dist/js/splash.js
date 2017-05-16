d6381f750b27d189480b030542e321135fb3111b
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-05-16 14:12:24 +0530

    display message on splash screen

--- web-dist/js/splash.js
+++ web-dist/js/splash.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 
 	setTimeout(function(){
 		$('body').addClass('loaded');
+		$('div').remove('.entry-header');
+		var element = document.getElementById("demo-content");
+		element.outerHTML = "";
+		delete element;
 	}, 3000);
 
 });
